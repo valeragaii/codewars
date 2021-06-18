5b2069b94246e8959fa3726d3cc6125f5b09ed15
valeragaii <66421815+valeragaii@users.noreply.github.com>
2021-06-18 20:52:23 +0200

    First commit

---
+++ Breaking_chocolate_problem.py
@@ -0,0 +1,12 @@
+"""Description:
+Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+
+For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
+
+If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). Input will always be a non-negative integer."""
+
+def break_chocolate(n, m):
+    if (n > 1 and m > 1) or (n > 1 and m == 1) or (n == 1 and m > 1):
+        return (m * n - 1)
+    else:
+	    return 0
